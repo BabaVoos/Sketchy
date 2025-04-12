5dfbeb5d4c3f2d8e9f95378cf70d165b1aef6a2f
BabaVoos <mudarabdulhadi@gmail.com>
2025-04-12 04:22:13 +0200

    Add splash screen configuration and initial screen implementations

--- lib/sketchy.dart
+++ lib/sketchy.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sketchy/features/home/ui/home_screen.dart';
 
 class Sketchy extends StatelessWidget {
   const Sketchy({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Sketchy', home: Placeholder());
+    return const MaterialApp(
+      title: 'Sketchy',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
